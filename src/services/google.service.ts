cfa38b31512c975a919626d38ef75be4f84a245f
BaraAttar <Barraatar@gmail.com>
2025-10-14 04:28:42 +0300

    fix: google auth

--- src/services/google.service.ts
+++ src/services/google.service.ts
@@ -1,8 +1,12 @@
 import { OAuth2Client, type TokenPayload } from 'google-auth-library';
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
-if (!GOOGLE_CLIENT_ID) {
+const WEB_CLIENT_ID = process.env.GOOGLE_WEB_CLIENT_ID!;
+const ANDROID_CLIENT_ID = process.env.GOOGLE_ANDROID_CLIENT_ID!;
+const IOS_CLIENT_ID = process.env.GOOGLE_IOS_CLIENT_ID!;
+
+if (!WEB_CLIENT_ID || !ANDROID_CLIENT_ID || !IOS_CLIENT_ID) {
     throw new Error("FATAL ERROR: GOOGLE_CLIENT_ID is not defined in environment variables");
 }
 
@@ -11,7 +15,7 @@ const client = new OAuth2Client();
 export async function verifyGoogleToken(idToken: string): Promise<TokenPayload | undefined> {
     const ticket = await client.verifyIdToken({
         idToken,
-        audience: [GOOGLE_CLIENT_ID].toString()
+        audience: [WEB_CLIENT_ID, ANDROID_CLIENT_ID, IOS_CLIENT_ID]
 
     });
     return ticket.getPayload() || undefined;
